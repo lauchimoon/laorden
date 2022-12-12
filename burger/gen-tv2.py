31cdd10ffa107548d0027098c2ac003909a9e4cf
catmanl <laurentinoluna06@protonmail.com>
2022-12-11 22:23:09 -0300

    finish burger tv2

---
+++ burger/gen-tv2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from moviepy.editor import *
+
+excel = pd.ExcelFile('../data.xlsx')
+data_burger = pd.read_excel(excel, 'Burger').to_dict()
+
+table = ImageClip('../out/burger1.png').set_duration(40).fadein(1).fadeout(1)
+clip1 = VideoFileClip('../clips/tv2-burger1.mp4')
+clip2 = VideoFileClip('../clips/tv2-burger2.mp4')
+clip3 = VideoFileClip('../clips/tv2-burger3.mp4')
+clip4 = VideoFileClip('../clips/tv2-burger4.mp4')
+
+result = concatenate_videoclips([
+    clip1, table, clip2, table, clip3, table, clip4
+])
+result.preview()
